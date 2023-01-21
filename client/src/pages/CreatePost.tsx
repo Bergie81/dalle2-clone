5b391166a7188e8a0028180aadfa6742dc4749da
Thomas Bergmann <bergie81@gmail.com>
2023-01-21 15:48:04 +0100

    Connect to OpenAI Api to create images

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -19,7 +19,29 @@ const CreatePost = () => {
 	const [loading, setLoading] = useState(false);
 
 	// FUNCTIONS
-	const generateImage = () => {};
+	const generateImage = async () => {
+		if (form.prompt) {
+			try {
+				setGeneratingImg(true);
+				const response = await fetch("http://localhost:8080/api/v1/dalle", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ prompt: form.prompt }),
+				});
+
+				const data = await response.json();
+				setForm({ ...form, image: `data:image/jpeg;base64,${data.image}` });
+			} catch (error) {
+				alert(error);
+			} finally {
+				setGeneratingImg(false);
+			}
+		} else {
+			alert("Please enter a prompt");
+		}
+	};
 	const handleSubmit = () => {};
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setForm({ ...form, [e.target.name]: e.target.value });
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,41 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configration, OpenAiApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openAi = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+	res.status(200).json({ message: "Hello from DALL-E!" });
+});
+
+router.route("/").post(async (req, res) => {
+	try {
+		const { prompt } = req.body;
+		console.log("PROMPT", prompt);
+
+		const aiResponse = await openAi.createImage({
+			prompt,
+			n: 1,
+			size: "1024x1024",
+			response_format: "b64_json",
+		});
+
+		const image = aiResponse.data.data[0].b64_json;
+		res.status(200).json({ image });
+	} catch (error) {
+		console.log("ERROR", error);
+		res
+			.status(500)
+			.send(error?.response.data.error.message || "Something went wrong");
+	}
+});
+
+export default router;
